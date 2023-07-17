8c1fcc9901478501acc425054f62fe63dfc75424
KoFE <petrovykh03@gmail.com>
2023-07-17 14:51:46 +0500

    Universal slider class added

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-import Slider from "./modules/slider";
+import Slider from "./modules/slider/slider";
 import VideoPlayer from "./modules/playVideo";
+import MainSlider from "./modules/slider/slider-main";
 
 window.addEventListener('DOMContentLoaded', () => {
-    const slider = new Slider('.page', '.next');
+    const slider = new MainSlider({page: '.page', btns: '.next'});
     slider.render();
     
     const player = new VideoPlayer('.showup .play', '.overlay');
