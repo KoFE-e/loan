67cb7d1506c2385825fc896b3f51db1fb241b511
KoFE <petrovykh03@gmail.com>
2023-07-17 14:38:33 +0500

    YT video player added

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,9 +15,25 @@ export default class Slider {
             this.slideIndex = this.slides.length;
         }
 
+        try {
+            this.hanson.style.opacity = '0';
+
+            if (n === 3) {
+                this.hanson.classList.add('animated');
+                setTimeout(() => {
+                    this.hanson.style.opacity = '1';
+                    this.hanson.classList.add('slideInUp');
+                }, 3000);
+            } else {
+                this.hanson.classList.remove('slideInUp');
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    
         this.slides.forEach(slide => {
             slide.style.display = "none";
-            slide.classList.add('animated', 'fadeInDown');
+            slide.classList.add('animated', 'slideInUp');
         });
 
         this.slides[this.slideIndex - 1].style.display = 'block';
@@ -28,6 +44,12 @@ export default class Slider {
     }
 
     render() {
+        try {
+            this.hanson = document.querySelector('.hanson');
+        } catch(e) {
+            console.log(e);
+        }
+        
         this.btns.forEach(btn => {
             btn.addEventListener('click', () => {
                 this.plusSlides(1);
